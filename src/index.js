70ac48550758216553aa227330545e3e652101a8
Bruno Bigotto <bbigotto@gmail.com>
2019-12-07 16:23:18 -0400

    Server side

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-const express = require('express')
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 // Setup static directory for serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send()
+io.on('connection', (socket) => {
+    console.log('New websocket connection')
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined')
+    socket.on('sendMessage', (msg, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(msg)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', msg)
+        callback('Delivered')
+    })
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+        callback('Location shared!')
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
 })
